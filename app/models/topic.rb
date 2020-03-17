922d8454784f2702225abb2e409b114e71094c18
shunsuke291 <s5t9n6n8s@docomo.ne.jp>
2020-03-17 12:13:10 +0000

    コメントモデルの関連を追加

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -6,4 +6,5 @@ class Topic < ApplicationRecord
    mount_uploader :image, ImageUploader
    has_many :favorites
   has_many :favorite_users, through: :favorites, source: 'user'
+   has_many :comments
 end
